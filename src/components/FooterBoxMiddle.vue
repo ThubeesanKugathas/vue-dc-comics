4b534d95d4b5142db3aa5eff516992c79e1ea49d
ThubeesanKugathas <Thubeesan@outlook.com>
2022-01-27 18:07:53 +0100

    fix FooterBoxMiddle template/style

--- src/components/FooterBoxMiddle.vue
+++ src/components/FooterBoxMiddle.vue
@@ -1,60 +1,64 @@
 <template>
     <div class="ms_ft-middle">
         <div class="ms_container">
+
+            <!-- left side flex container -->
             <div class="ms_left">
                 <ul class="ms_list">
                     <div class="ms_first-columns">
                         <li>
                             <p>DC COMICS</p>
                             <ul>
-                                <li>Character</li>
-                                <li>Character</li>
-                                <li>Character</li>
-                                <li>Character</li>
-                                <li>Character</li>
-                                <li>Character</li>
-                                <li>Character</li>
+                                <li><a href="#/">Character</a></li>
+                                <li><a href="#/">Comics</a></li>
+                                <li><a href="#/">Movie</a></li>
+                                <li><a href="#/">TV</a></li>
+                                <li><a href="#/">Games</a></li>
+                                <li><a href="#/">Videos</a></li>
+                                <li><a href="#/">News</a></li>
                             </ul>
                         </li>
 
                         <li>
-                            <p>DC COMICS</p>
+                            <p>SHOP</p>
                             <ul>
-                                <li>Character</li>
-                                <li>Character</li>
+                                <li><a href="#/">Shop DC</a></li>
+                                <li><a href="#/">Shop DC Collectibles</a></li>
                             </ul>
                         </li>
                     </div>
 
                     <li>
-                        <p>DC COMICS</p>
+                        <p>DC</p>
                         <ul>
-                            <li>Character</li>
-                            <li>Character</li>
-                            <li>Character</li>
-                            <li>Character</li>
-                            <li>Character</li>
-                            <li>Character</li>
-                            <li>Character</li>
-                            <li>Character</li>
-                            <li>Character</li>
-                            <li>Character</li>
-                            <li>Character</li>
+                            <li><a href="#/">Terms of use</a></li>
+                            <li><a href="#/">Privacy policy (New)</a></li>
+                            <li><a href="#/">Ad Choices</a></li>
+                            <li><a href="#/">Advertising</a></li>
+                            <li><a href="#/">Jobs</a></li>
+                            <li><a href="#/">Subscriptions</a></li>
+                            <li><a href="#/">Talent Workshop</a></li>
+                            <li><a href="#/">CPSC Certificates</a></li>
+                            <li><a href="#/">Ratings</a></li>
+                            <li><a href="#/">Shop Help</a></li>
+                            <li><a href="#/">Contact Us</a></li>
                         </ul>
                     </li>
 
                     <li>
-                        <p>DC COMICS</p>
+                        <p>SITES</p>
                         <ul>
-                            <li>Character</li>
-                            <li>Character</li>
-                            <li>Character</li>
-                            <li>Character</li>
-                            <li>Character</li>
+                            <li><a href="#/">DC</a></li>
+                            <li><a href="#/">MAD Magazine</a></li>
+                            <li><a href="#/">DC Kids</a></li>
+                            <li><a href="#/">DC Universe</a></li>
+                            <li><a href="#/">DC Power Visa</a></li>
                         </ul>
                     </li>
                 </ul>
             </div>
+
+            <!-- right side flex container -->
             <img src="../assets/dc-logo-bg.png" alt="dc-logo-bg">
         </div>
     </div>
@@ -73,17 +77,39 @@ export default {
         height: 400px;
         background: url(../assets/footer-bg.jpg) no-repeat;
         background-size: cover;
-        color: #fff;
 
         .ms_container {
             width: 1200px;
             margin: 0 auto;
             display: flex;
+            justify-content: space-between;
+            align-items: center;
 
+            // left side -------------------------------
             .ms_list {
                 display: flex;
                 max-height: 100%;
+
+                li {
+                    margin-right: 20px;
+                }
+
+                p {
+                    color: $white-color;
+                    font-size: 20px;
+                    font-weight: bold;
+                    margin: 10px 0;
+                }
+
+                a {
+                    text-decoration: none;
+                    color: #888;
+                }
+                
+
             }
+
+            // right side ------------------------------
             img {
                 height: 400px;
             }
