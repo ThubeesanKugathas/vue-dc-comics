2676c5ce41155d4ac4f64d895a6a21bc4dde36c6
ThubeesanKugathas <Thubeesan@outlook.com>
2022-01-27 14:53:57 +0100

    downloaded images + creations main components

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,27 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header-box />
+    <main-box />
+    <footer-box />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import HeaderBox from './components/HeaderBox.vue'
+  import MainBox from './components/MainBox.vue'
+  import FooterBox from './components/FooterBox.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HeaderBox,
+    MainBox,
+    FooterBox,
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
