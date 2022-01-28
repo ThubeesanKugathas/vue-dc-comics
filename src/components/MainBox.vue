669b3318162dbec25e44c9f499bfa726cb0d4fd1
ThubeesanKugathas <Thubeesan@outlook.com>
2022-01-28 13:08:36 +0100

    MainBox add tags/button

--- src/components/MainBox.vue
+++ src/components/MainBox.vue
@@ -1,6 +1,9 @@
 <template>
     <main>
         <div class="ms_container">
+            <div class="ms_current-series">
+                <h2>CURRENT SERIES</h2>
+            </div>
             <div v-for="(card, index) in comicsList" :key="index"
                 class="ms_comics-card">
                 <div class="ms_card-image">
@@ -9,6 +12,12 @@
                 <p>{{card.series.toUpperCase()}}</p>
             </div>
         </div>
+
+        <span>
+            <a href="#/">
+                LOAD MORE
+            </a>
+        </span>
     </main>
 </template>
 
@@ -26,14 +35,23 @@ export default {
     main {
         background-color: #222;
         color: $white-color;
-        padding: 30px 0;
+        padding: 50px 0 30px;
 
         .ms_container {
             width: 1200px;
             margin: 0 auto;
             display: flex;
-            justify-content: space-between;
+            justify-content: space-around;
             flex-wrap: wrap;
+            position: relative;
+
+            .ms_current-series {
+                position: absolute;
+                top: -70px;
+                left: 0%;
+                background-color: $blue-color;
+                padding: 10px 15px;
+            }
 
             .ms_comics-card {
                 width: calc((100% - 150px) / 6);
@@ -46,6 +64,20 @@ export default {
                 p {
                     margin: 20px 0 30px;
                 }
+            }
+        }
+
+        span {
+            padding: 10px 50px;
+            background-color: $blue-color;
+            display: table;
+            margin: 0 auto;
+            font-weight: bold;
+            cursor: pointer;
+
+            a {
+                text-decoration: none;
+                color: $white-color;
             }
         }
     }
